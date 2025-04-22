7b0a0c83c7f4a3be88aad91713614ede240d613e
JIMBE <adriyansyahmf0@gmail.com>
2025-04-22 09:18:58 +0700

    hmmmmm....

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['i1.wp.com', 'oploverz.my', 'i3.wp.com', 'i2.wp.com'], // Add the external domain here
+    domains: ['i1.wp.com', 'oploverz.my', 'i3.wp.com', 'i2.wp.com'], // External domains
   },
-  
+  eslint: {
+    ignoreDuringBuilds: true, // Nonaktifkan ESLint selama build
+  },
 };
 
 export default nextConfig;
